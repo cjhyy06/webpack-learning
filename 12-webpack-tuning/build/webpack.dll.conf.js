f4709ce2b3e61f2388ff0a5635da0a27bfc8546f
xuesong <664395401@qq.com>
2019-06-23 17:42:48 +0800

    .

---
+++ 12-webpack-tuning/build/webpack.dll.conf.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+    entry: {
+        vue: ['vue', 'vue-router'],
+        ui: ['element-ui']
+    },
+
+    output: {
+        path: path.join(__dirname, '../src/dll/'),
+        filename: '[name].dll.js',
+        library: '[name]'
+    },
+
+    plugins: [
+        new webpack.DllPlugin({
+            path: path.join(__dirname, '../src/dll/', '[name]-mainifest.json'),
+            name: '[name]'
+        }),
+        
+        new webpack.optimize.UglifyJsPlugin()
+    ]
+}
